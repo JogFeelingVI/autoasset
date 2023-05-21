78e032c477d36a4486143c064c2f91ddf07e576e
JogfeeingVI <lifelse@outlook.com>
2023-05-21 09:55:08 +0800

    updte

--- generate_asset.py
+++ generate_asset.py
@@ -2,7 +2,7 @@
 # @Author: JogFeelingVI
 # @Date:   2023-05-15 20:22:04
 # @Last Modified by:   JogFeelingVI
-# @Last Modified time: 2023-05-21 09:28:02
+# @Last Modified time: 2023-05-21 09:54:54
 from operator import ge
 from pickletools import markobject
 import re, json
@@ -12,28 +12,28 @@ from Codex import gethtml, pathliab, glns
 
 
 class assetx:
-    error:bool = False
-    
+    error: bool = False
+
     @staticmethod
-    def anyishtml(text:str) -> list:
+    def anyishtml(text: str) -> list:
         if text in ['', None]:
             return []
         pattern = re.compile(r'<tr[^>]*>(.*?)</tr>', re.DOTALL)
         match = pattern.findall(text)
-        Rx =[]
-        Bx =[]
+        Rx = []
+        Bx = []
         if match:
             for _match in match:
                 td = re.compile(r'<td>[\d]{7}</td>')
                 if td.findall(_match):
                     num_r = re.compile(r'ball_5\'>([\d]{2})</span')
-                    numsr  = num_r.findall(_match)
+                    numsr = num_r.findall(_match)
                     num_b = re.compile(r'ball_1\'>([\d]{2})<span')
                     numob = num_b.findall(_match)
                     Rx.extend(numsr)
                     Bx.extend(numob)
         return [Rx, Bx]
-    
+
     def getnetdate(self):
         try:
             asset_json = pathliab.ospath.file_path('./asset.json')
@@ -41,8 +41,7 @@ class assetx:
                 return
             nat_dev = 'https://chart.cp.360.cn/kaijiang/ssq'
             html_content = gethtml.get_html(nat_dev).neirong
-            
-            
+
             if html_content != '':
                 # Rx = re.findall(r'>([0-9,]{17})<', html_content)
                 # Bx = re.findall(r'c_bule\">([0-9]{2})<', html_content)
@@ -52,7 +51,7 @@ class assetx:
                     'B': [int(x) for x in Bx],
                     'date': dtime.now().__str__()
                 }
-                
+
                 json_str = json.dumps(self.Lix, indent=0)
                 with open(asset_json, 'w') as datajson:
                     datajson.write(json_str)
@@ -63,41 +62,41 @@ class assetx:
         except Exception as e:
             print(f'error: {e}')
             self.error = True
-    
-    @staticmethod      
-    def listTostr(lis:list) -> str:
+
+    @staticmethod
+    def listTostr(lis: list) -> str:
         prompt = '█'
-        if lis.__len__() ==0:
+        if lis.__len__() == 0:
             return 'No Numbers'
         temp_lis = [[i, lis.count(i)] for i in set(lis)]
         temp_sort = sorted(temp_lis, key=lambda x: x[1], reverse=True)
-        temp:str = '\n'.join([f' - {n:>02} Count {c:>2} {prompt * c}' for n, c in temp_sort])
+        temp: str = '\n'.join(
+            [f' - {n:>02} Count {c:>2} {prompt * c}' for n, c in temp_sort])
         return temp
-    
+
     def shownumber(self) -> str:
         r = self.Lix['R'][:6]
         b = self.Lix['B'][0]
         r_str = ' '.join([f'{x:02}' for x in r])
         return f'*{r_str} / {b:02}*'
-    
-    def groupBysix(self) ->List[str]:
+
+    def groupBysix(self) -> List[str]:
         data = self.Lix.get('R', [])
         groups = [data[i:i + 6] for i in range(0, len(data), 6)]
         groups = groups[::-1]
         matrix = []
-        
+
         for i in range(1, 34):
             row = ''
             for g in groups:
                 if i in g:
-                    row += '▮'
+                    row += '■'
                 else:
-                    row += '-'
+                    row += '□'
             matrix.append(row)
 
         return matrix
 
-            
     def tomd(self):
         try:
             if self.error:
@@ -108,7 +107,7 @@ class assetx:
                 return
             markdown.append('## Auto update asasset.json')
             markdown.append(f' - update {self.Lix.get("date", "None")}')
-            markdown.append(f'### Black box number {self.shownumber()}')
+            markdown.append(f' - Black box number {self.shownumber()}')
             matrix = self.groupBysix()
             markdown.append('#### Sequence graphics')
             for i, row in enumerate(matrix):
@@ -117,18 +116,21 @@ class assetx:
             # markdown.append(f'{self.listTostr(self.Lix.get("R", []))}')
             # markdown.append('#### Blue ball list')
             # markdown.append(f'{self.listTostr(self.Lix.get("B", []))}')
-            markdown.append('## creativity list')
-            for x in (0, 0, 0, 0, 0, 1, 0, 0, 0, 0 ,0,1,0,0,0,0,0,1,0,0,0,0,0):
+            markdown.append('## Creativity list')
+            for x in (0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
+                      0, 0, 0, 0):
                 if x == 1:
                     markdown.append('---')
                 else:
-                    markdown.append(f'- [x] {glns.glnsMpls(self.Lix).creativity()}')
+                    markdown.append(
+                        f'- [x] {glns.glnsMpls(self.Lix).creativity()}')
             readme_path = pathliab.Path(readme_md)
             with readme_path.open(mode='w') as wMd:
                 for line in markdown:
                     wMd.write(f'{line}\n')
         except:
             print(f'An exception occurred while writing readme.md')
+
 
 def main():
     assx = assetx()
