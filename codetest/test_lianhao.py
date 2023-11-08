71337abc71a4599a1cea089d642cacd6525ae455
JogfeeingVI <lifelse@outlook.com>
2023-11-09 06:12:45 +0800

    Update test_lianhao.py

--- codetest/test_lianhao.py
+++ codetest/test_lianhao.py
@@ -2,7 +2,7 @@
 # @Author: JogFeelingVI
 # @Date:   2023-10-20 14:47:22
 # @Last Modified by:   JogFeelingVI
-# @Last Modified time: 2023-11-07 21:17:33
+# @Last Modified time: 2023-11-08 22:00:34
 # from Codex.datav import LoadJson
 # from Codex.rego import rego
 from collections import Counter, deque
@@ -15,21 +15,25 @@ import unittest, time, os
 
 
 def runtime(func):
+
     def inst(*args, **kwargs):
         t1 = time.time()
         for i in range(1000 * 10):
             rex = func(*args, **kwargs)
         t2 = time.time()
         print(f'    -> Run time {t2-t1:.4f}s')
+
     return inst
 
+
 @runtime
-def cosv(x:List, y:List):
-    dot = sum(a*b for a, b in zip(x, y))
-    normx = math.sqrt(sum([a*a for a in x]))
-    normy = math.sqrt(sum([a*a for a in y]))
+def cosv(x: List, y: List):
+    dot = sum(a * b for a, b in zip(x, y))
+    normx = math.sqrt(sum([a * a for a in x]))
+    normy = math.sqrt(sum([a * a for a in y]))
     cos = dot / (normx * normy)
     print(f'COS {cos}')
+
 
 def dzx():
     print('glns test')
@@ -41,11 +45,11 @@ def dzx():
     reeego.parse_v2()
     filters.Lever = glns.getabc
     filters.Last = glns.getlast
-    
+
     count = 0
     while True:
         # n = glns 是最慢的
-        n=glns.creativity()
+        n = glns.creativity()
         # = True
         for k, func in filters.filters.items():
             if func(n) == False:
@@ -66,7 +70,7 @@ class lianhaotest(unittest.TestCase):
     def setUp(self) -> None:
         self.a = [10, 24, 26, 28, 29, 31]
         self.b = [3, 8, 19, 22, 26, 32]
-        self.c = [11,15,18,24,26,32]
+        self.c = [11, 15, 18, 24, 26, 32]
         return super().setUp()
 
     def test_linma(self):
